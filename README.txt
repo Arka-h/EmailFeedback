d4cf7170aefbf097efb9f9b5c5f077573ed26b92
Arka-h <aurkohaldi@gmail.com>
2020-01-04 00:20:28 +0530

    commit2

--- README.txt
+++ README.txt
@@ -4,3 +4,13 @@ https://git.heroku.com/morning-sea-25305.git : git remote add heroku [git_link]
 https://morning-sea-25305.herokuapp.com/ 
 
 morning-sea-25305
+
+>> heroku logs show the changes on heroku
+
+>>>> We'll use the OAuth authentication standard
+>>>> Helper library that helps in the OAuth process ==> Passport.js
+
+>>>> Generate Client ID and Password : 
+>>>> ID : 197216956050-00iqehjqsl4bob61gshqgu7ovai5vn4u.apps.googleusercontent.com  
+//identifies our application with google and can be shared
+>>>> secret : 3yklgJjxjrdv1vkSJmI5pKl5
--- index.js
+++ index.js
@@ -1,22 +1,39 @@
 //import express from "express";
-const express = require("express");
+const express = require("express"); //express is a backend api built on Nodejs
+const passport = require("passport")//passport is api that helps in authentication process of various services
+const googleStrategy = require("passport-google-oauth20").Strategy// this is the google oauth 'strategy'
 const app = express(); //returns app  object
 
-app.get("/", (req, res) => {// req ==>  request from the client
-    // res ==> response that you will send out to the client
-  res.send({
-    bye: "buddy"
-  });
-});
-// Get env variable PORT from env on which Node runs, and Heroku modifies/creates
-const PORT = process.env.PORT || 5000
-app.listen(PORT); // express tells node to watch for traffic on port 5000
-// we can use AWS/DigitalOcean for hosting
 
-//TODO : dependencies : tell package what version node you want to use "engines"
-//TODO : specify command to start our server: "scripts"
+//TODO : Create a new Google Strategy , and add it to the passport register
+passport.use(new googleStrategy())
+//TODO : Create a profile with google and enaable G+ API for the Oauth feature
+
+
+
+// app.get("/", (req, res) => {// req ==>  request from the client
+//     // res ==> response that you will send out to the client
+//   res.send({
+//     bye: "buddy"
+//   });
+// });
+
+
+// Get env variable PORT from env on which Node runs, and Heroku modifies/creates
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT); // express tells nookde to watch for traffic on port 5000
+
+
+// we can use AWS/DigitalOcean for hosting instead of Heroku...
+
+//TODO : (in package.json) dependencies : tell package what version node you want to use "engines"
+//TODO : (in package.json) specify command to start our server: "scripts"
 //TODO : The "node_modules" depndencies are auto-installed that are 
 // set up as a consequence to the dependencies:express option in package.json
 // Hence, they aren't required in the deployment/VersionControl and should be ignored using the ".gitignore"
+
+//TODO : deploy using Heroku CLI, and Git 
 
 
