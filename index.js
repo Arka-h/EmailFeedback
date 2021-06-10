95a6741ef7b8b470efc46a0fd04ae12bcffd437d
Arka Haldi <47897466+Arka-h@users.noreply.github.com>
2021-06-10 22:37:27 +0530

    Setting up Heroku

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 5000
+// Get env from Heroku's Cloud env || assign 5000
+// For Heroku deployment, add the engines prop with npm and node attributes set to version
 
 app.get('/',(req,res)=>{
     res.send("HI, I'm working")
 })
 
-app.listen(5000);
+app.listen(PORT);
 //node index.js
