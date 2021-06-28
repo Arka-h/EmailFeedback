91ae5b50746950a7351c6d0cb07dc2f77ad36908
Arka Haldi <47897466+Arka-h@users.noreply.github.com>
2021-06-28 21:04:12 +0530

    Added final changes to render surveys

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -10,7 +10,27 @@ const Surveylist = (props) => {
     }, [])
     return (
         <>
-            {/* {surveys} */}
+            <div className="flex-container">
+                {surveys.reverse().map(({ body, title, subject, dateSent, response }) => <>
+                    <div className="row">
+                        <div className="col s12 m6">
+                            <div className="card ">
+                                <div className="card-content">
+                                    <span className="card-title"><b><h5>{subject}</h5></b></span>
+                                    <b>{title}</b>
+                                    <p>{body}</p>
+                                    <small className="right">
+                                        Sent On: {new Date(dateSent).toLocaleDateString()}
+                                    </small>
+                                </div>
+                                <div className="card-action" >
+                                    {response ? Object.entries(response).map(([key, val]) => <a>{key}: {val}</a>): null}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </>)}
+            </div>
         </>
     )
 }
